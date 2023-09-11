a00cf8a43046b7240b364c43360b2df53e6eb64d
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-11 14:06:56 -0300

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,21 +6,6 @@ const productController = {
     const products = await Product.find().populate("category");
     return res.json(products);
   },
-  findByCat: async (req, res) => {
-    try {
-      const paramsCat = req.params.category;
-      const category = await Category.findOne({ name: paramsCat }).populate(
-        "products"
-      );
-      const products = [];
-      for (let i = 0; i < category.products.length; i++) {
-        products.push(category.products[i]);
-      }
-      return res.json(products);
-    } catch (error) {
-      return res.json("Error en parámetros de búsqueda");
-    }
-  },
   show: (req, res) => {
     console.log("llegamos a show");
   },
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,6 @@ const productController = require("../controllers/productController");
 const productRouter = express.Router();
 
 productRouter.get("/", productController.index);
-<<<<<<< Updated upstream
 productRouter.get("/:id", productController.show);
 
-=======
-productRouter.get("/:category", productController.findByCat);
-productRouter.get("/:id", productController.show);
->>>>>>> Stashed changes
 module.exports = productRouter;
