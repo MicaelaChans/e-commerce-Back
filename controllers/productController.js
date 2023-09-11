ffabb8204b6a42ec55f92d7adcba9049447ae3a3
MachinYDraco <fede.mier.2000@gmail.com>
2023-09-11 13:59:33 -0300

    show

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,14 @@
 const Product = require("../models/Product");
-const Category = require("../models/Category")
+const Category = require("../models/Category");
 
 const productController = {
   index: async (req, res) => {
     const products = await Product.find().populate("category");
     return res.json(products);
   },
-  show: async (req, res) =>{
-    return res.json("Holis");
+  show: async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
   },
 };
 
