658f84089f511fe5263f9188e5a3dd6ca1700ad6
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-11 14:10:09 -0300

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,21 +6,11 @@ const productController = {
     const products = await Product.find().populate("category");
     return res.json(products);
   },
-  show: (req, res) => {
-    console.log("llegamos a show");
+  show: async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    return res.json(product);
   },
 };
 
 module.exports = productController;
 
-//  show: async (req, res) => {
-//    console.log("llegamos");
-//    try {
-//      const product = await Product.findById(req.params.id);
-//      if (!product) {
-//        return res.status(404).json({ message: "Producto no encontrado" });
-//      }
-//      res.json(product);
-//    } catch (error) {
-//      return res.json("Error en parámetros de búsqueda");
-//    }
