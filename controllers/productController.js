a4adcd54a4b51989972db6f47c05769915c4aa5e
MachinYDraco <fede.mier.2000@gmail.com>
2023-09-11 14:01:13 -0300

    ahora

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,10 +6,42 @@ const productController = {
     const products = await Product.find().populate("category");
     return res.json(products);
   },
+<<<<<<< Updated upstream
   show: async (req, res) => {
     const product = await Product.findById(req.params.id);
     return res.json(product);
+=======
+  findByCat: async (req, res) => {
+    try {
+      const paramsCat = req.params.category;
+      const category = await Category.findOne({ name: paramsCat }).populate(
+        "products"
+      );
+      const products = [];
+      for (let i = 0; i < category.products.length; i++) {
+        products.push(category.products[i]);
+      }
+      return res.json(products);
+    } catch (error) {
+      return res.json("Error en parámetros de búsqueda");
+    }
+  },
+  show: (req, res) => {
+    console.log("llegamos a show");
+>>>>>>> Stashed changes
   },
 };
 
 module.exports = productController;
+
+//  show: async (req, res) => {
+//    console.log("llegamos");
+//    try {
+//      const product = await Product.findById(req.params.id);
+//      if (!product) {
+//        return res.status(404).json({ message: "Producto no encontrado" });
+//      }
+//      res.json(product);
+//    } catch (error) {
+//      return res.json("Error en parámetros de búsqueda");
+//    }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,11 @@ const productController = require("../controllers/productController");
 const productRouter = express.Router();
 
 productRouter.get("/", productController.index);
+<<<<<<< Updated upstream
 productRouter.get("/:id", productController.show);
 
+=======
+productRouter.get("/:category", productController.findByCat);
+productRouter.get("/:id", productController.show);
+>>>>>>> Stashed changes
 module.exports = productRouter;
