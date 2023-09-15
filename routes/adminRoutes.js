da412aaa22966bcfca2e6b64ea24d7f62b47afd5
Micaela Chans <micaelachansmoure@gmail.com>
2023-09-15 19:04:11 -0300

    adminController

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const adminController = require("../controllers/adminController");
-const authController = require("../controllers/authController");
 const router = express.Router();
 
+//router.post("/login", adminController.login);
+
 router.get("/", adminController.index);
-//router.post("/login", authController.login);
-router.post("/logout", authController.logout);
+router.post("/", adminController.create);
+router.get("/:id", adminController.show);
+router.patch("/:id", adminController.update);
+router.delete("/:id", adminController.destroy);
+
+router.post("/logout", adminController.logout);
 
 module.exports = router;
