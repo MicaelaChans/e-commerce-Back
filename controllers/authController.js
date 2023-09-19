e9b5941de786092033c1de82e2d89a7483553171
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-19 12:24:22 -0300

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const authController = {
     if (!user) return res.status(401).json({ error: "Wrong credentials..." });
 
     //Verificar contraseña (la contraseña en db hace match con la recibida)
-    const validatePassword = user.password// await bcrypt.compare(password, user.password);
+    const validatePassword =  await bcrypt.compare(password, user.password);
 
     if (!validatePassword) return res.status(401).json({ error: "Wrong credentials..." });
 
