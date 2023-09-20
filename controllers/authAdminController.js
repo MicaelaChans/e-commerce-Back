919fefd07c4ef888495caf52c1cda7a5bf7cac76
Agustin Martinez <77639848+Griperlight@users.noreply.github.com>
2023-09-20 19:09:36 -0300

    Updated authAdmin

--- controllers/authAdminController.js
+++ controllers/authAdminController.js
@@ -3,40 +3,37 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const authAdminController = {
-  tokensAdmin: async (req, res) => {
-    console.log("tamo en vivo");
-    const { username, password } = req.body;
-    //Validacion básica
-    if (!username || !password) {
-      return res
-        .status(400)
-        .json({ error: "Please provide both username and password." });
-    }
-    //Verificamos credenciales
-    //Verificar usuario en DB
-    const admin = await Admin.findOne({ username: req.body.username });
+    tokensAdmin: async (req, res) => {
+        const { username, password } = req.body;
+        console.log("Datos de entrada:", username, password);
+        if (!username || !password) {
+            return res
+                .status(400)
+                .json({ error: "Please provide both username and password." });
+        }
+        const admin = await Admin.findOne({ username: req.body.username });
 
-    if (!admin) return res.status(401).json({ error: "Wrong credentials..." });
+        if (!admin) {
+            return res.status(401).json({ error: "User not found." });
+        }
 
-    //Verificar contraseña (la contraseña en db hace match con la recibida)
-    const validatePassword = await bcrypt.compare(password, admin.password);
+        const validatePassword = await bcrypt.compare(password, admin.password);
+        if (!validatePassword) {
+            return res.status(401).json({ error: "Invalid password." });
+        }
 
-    if (!validatePassword)
-      return res.status(401).json({ error: "Wrong credentials..." });
+        const token = jwt.sign(
+            { sub: admin.id, username: admin.username },
+            process.env.JWT_SECRET
+        );
+        //Respondo con el token a la llamada
+        return res.status(200).json({ token });
+    },
 
-    //Genero token
-    const token = jwt.sign(
-      { sub: admin.id, username: admin.username },
-      process.env.JWT_SECRET
-    );
-    //Respondo con el token a la llamada
-    return res.status(200).json({ token });
-  },
-
-  logout: async (req, res) => {
-    res.json("Entraste en Login (backend)");
-    return console.log("Admin hizo logout!");
-  },
+    logout: async (req, res) => {
+        res.json("Entraste en Login (backend)");
+        return console.log("Admin hizo logout!");
+    },
 };
 
 module.exports = authAdminController;
