a9efd38782e1f8235d0981b4972249c6cae028d7
MartinGestido <89423775+MartinGestido@users.noreply.github.com>
2023-09-22 12:53:46 -0300

    .

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const checkRole = require("../middlewares/checkRole");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-router.get("/", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), checkRole('admin'), adminController.index);
-router.post("/", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), checkRole('admin'), adminController.create);
-router.get("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), checkRole('admin'), adminController.show);
-router.patch("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), checkRole('admin'), adminController.update);
-router.delete("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), checkRole('admin'), adminController.destroy);
+router.get("/", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),  adminController.index);
+router.post("/", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),  adminController.create);
+router.get("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),  adminController.show);
+router.patch("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),  adminController.update);
+router.delete("/:id", checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),  adminController.destroy);
 
 module.exports = router;
